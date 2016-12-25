464a0e0f0782485e5c542dd53422f05e88c78187
Zsolt Kulcsar <zskulcsar@gmail.com>
2016-12-25 15:59:24 +0000

    Working on the timer

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class playercontroller : MonoBehaviour {
-    private static float multi = 5.0f;
+    private static float FORCE_MULTI = 5.0f;
     private static int PICKUPS_LAYER = 8;
 
     private static float EXP_FORCE = 15.0f;
@@ -20,8 +20,8 @@ public class playercontroller : MonoBehaviour {
     {
         if (rb.position.y < 0.6) // ie.: if we are not in the "air"
         {
-            float moveHorizontal = Input.GetAxis("Horizontal") * multi;
-            float moveVertical = Input.GetAxis("Vertical") * multi;
+            float moveHorizontal = Input.GetAxis("Horizontal") * FORCE_MULTI;
+            float moveVertical = Input.GetAxis("Vertical") * FORCE_MULTI;
 
             Vector3 force = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
